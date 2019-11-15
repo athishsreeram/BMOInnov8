33ed5f6e9eb4fe37b42eb65b67003d5702351391
anharay <athishsreeram.nharayanan@bmo.com>
2019-11-15 18:01:09 -0500

    Updated Flow 2 GAS

--- bmoinnov8.js
+++ bmoinnov8.js
@@ -13,6 +13,11 @@ var base_url = window.location.origin;
             window.location = base_url+"/BMOInnov8/com.html" ;
 }
 
+function gas() {
+var base_url = window.location.origin;
+            window.location = base_url+"/BMOInnov8/gas.html" ;
+}
+
 function simpleGet(){
 
     $.get("https://jsonplaceholder.typicode.com/posts/1",
@@ -20,4 +25,34 @@ function simpleGet(){
       alert("Data: " + data + "\nStatus: " + status);
     });
 
+}
+
+var addMarkersToMap = function(map, lat, lag,name) {
+		var latitudeAndLongitudeOne = new google.maps.LatLng(lat, lag);
+
+		var infowindow = new google.maps.InfoWindow({
+			content : name
+		});
+
+		var markerOne = new google.maps.Marker({
+			position : latitudeAndLongitudeOne,
+			map : map,
+			title : name
+		});
+
+		google.maps.event.addListener(markerOne, 'click', function() {
+			infowindow.open(map, markerOne);
+		});
+
+}
+
+var showMap = function(lat,lag) {
+    var mapOptions = {
+        zoom : 17,
+        center : new google.maps.LatLng(lat, lag),
+        mapTypeId : google.maps.MapTypeId.ROADMAP
+    }
+    var map = new google.maps.Map(document.getElementById("map_canvas"),
+            mapOptions);
+    return map;
 }
