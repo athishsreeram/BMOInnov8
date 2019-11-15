4ef48eb167cd36cb3259a11dcdd90a16fb24e8ea
anharay <athishsreeram.nharayanan@bmo.com>
2019-11-14 23:49:20 -0500

    Base

--- bmoinnov8.js
+++ bmoinnov8.js
@@ -1,38 +1,16 @@
-function wentto() {
-            window.location = "file:////Users/anharay/IdeaProjects/BMOInnov8/wantto.html" ;
+function home() {
+            window.location = "http://localhost/BMOInnov8/index.html" ;
 }
 
-function simplePost(){
-		/**$.ajax({
-			type : "POST",
-			//the url where you want to sent the userName and password to
-			url : appurl+"circles.php",
-			dataType : 'json',
-			async : false,
-			//json object to sent to the authentication url
-			data : {
-				userdetail : '{"username": "' + userName + '", "password" : "'
-						+ password + '"}',
-				login : "yes"
-			},
-			success : function(data) {
-				if (data.username != "") {
-					$.mobile.loading("hide");
-					console.timeEnd("concatenation");
-					window.localStorage.setItem("userdetail", JSON
-							.stringify(data));
-					navigator.app
-							.loadUrl("file:///android_asset/www/home.html");
-				}
-			},
-			error : function(data) {
-				$.mobile.loading("hide");
-				console.log(data);
-				if (typeof data.responseText != 'undefined') {
-					$("#error").html("Error in login " + data.responseText);
-				} else {
-					$("#error").html("Trun On Your Internet");
-				}
-			}
-		})*/
+function wentto() {
+            window.location = "http://localhost/BMOInnov8/wantto.html" ;
+}
+
+function simpleGet(){
+
+    $.get("https://jsonplaceholder.typicode.com/posts/1",
+    function(data,status){
+      alert("Data: " + data + "\nStatus: " + status);
+    });
+
 }
